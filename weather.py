840f1e7cf73ee154e3b5ecb45df254a5d8fd0f72
StarOfTheMoon <sarahabderemane@gmail.com>
2018-03-04 21:07:54 +0100

    change api data

--- weather.py
+++ weather.py
@@ -1,31 +1,34 @@
+import requests
 from flask import Flask
 from flask import request, render_template
-from apixu.client import ApixuClient, ApixuException
 
 
 app = Flask(__name__)
-
-api_key = 'fa6bb918d26a48d9954143058180502'
-client = ApixuClient(api_key)
+api_key = '12d601c64703986d2035b59334bfa3bb'
+url_google = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input="
+url_weather = "http://api.openweathermap.org/data/2.5/weather?"
+gkey = "AIzaSyCpQ9JYs7L8EKaOwOIl_deELvYaOviWPw0"
 
 # current weather
 @app.route('/')
 def index(current=None):
-	# geolocalisation
-	def geolocalisation():
-		print('coucou')
-	geolocalisation()
-	current = client.getCurrentWeather(q='Paris')
+	current = requests.get(url_weather +"q=Paris&APPID="+api_key+"&units=metric&lang=fr").json()
+	print(current)
 	return render_template('index.html', current=current)
 
 @app.route('/search', methods=['POST','GET'])
 def search(current=None):
+	city = ""
+	predictions = [];
 	if request.method == 'POST':
 		if request.form['city']:
 			city = request.form['city']
-	current = client.getCurrentWeather(q=city)
+	else: 
+		city = "Paris"
+	current = requests.get(url_weather+"q="+city+"&APPID="+api_key+"&units=metric&lang=fr").json()
+	print(current)
 	return render_template('index.html', current=current)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8000)
+    app.run(debug=True,host='0.0.0.0',port=8001)
 
